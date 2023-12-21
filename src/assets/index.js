f65431e2e172ae883847596001867f4b109b3546
Hattrick212 <tikitaka.mou@gmail.com>
2023-12-21 07:30:13 +0200

    fist commit

---
+++ src/assets/index.js
@@ -0,0 +1,155 @@
+import logo from "./logo.png";
+
+// socials
+import facebook from "./socials/facebook.png";
+import facebook1 from "./socials/facebook (1).png";
+import github from "./socials/github.png";
+import github1 from "./socials/github (1).png";
+import xing from "./socials/xing.png";
+import xing1 from "./socials/xing1.png";
+import linkedin from "./socials/linkedin.png";
+import linkedin1 from "./socials/linkedin (1).png";
+import twitter from "./socials/twitter.png";
+import twitter1 from "./socials/twitter (1).png";
+import instagram from "./socials/instagram.png";
+import instagram1 from "./socials/instagram (1).png";
+
+// projects
+import tvshowbox from "./projects/tvshow.png";
+import guesstheword from "./projects/guesstheword.png";
+import tictactoe from "./projects/tictactoe.png";
+import mathmagician from "./projects/mathmagician.png";
+import metricwebapp from "./projects/metricswebapp.png";
+import spacetravelers from "./projects/spacetravelers.png";
+import recipeapp from "./projects/recipeapp.png";
+import trackbudget from "./projects/trackbudget.png";
+import carrental from "./projects/carrental.png";
+import eva from "./projects/eva.png";
+import ados from "./projects/ados.jpg";
+import locks from "./projects/locks.png";
+import runn from "./projects/runn.png";
+import priojon from "./projects/priojon.png";
+import letsusedata from "./projects/letsusedata.png";
+
+// carousel img
+import guesstheword1 from "./carousels/guess the word screenshot 1.png";
+import guesstheword2 from "./carousels/guess the word screenshot 2.png";
+import guesstheword3 from "./carousels/guess the word screenshot 3.png";
+import spacetravelers1 from "./carousels/space travelers screenshot 1.png";
+import spacetravelers2 from "./carousels/space travelers screenshot 2.png";
+import spacetravelers3 from "./carousels/space travelers screenshot 3.png";
+import tictactoe1 from "./carousels/tic tac toe screenshot 1.png";
+import tictactoe2 from "./carousels/tic tac toe screenshot 2.png";
+import tictactoe3 from "./carousels/tic tac toe screenshot 3.png";
+import tvshow1 from "./carousels/tvshow screenshot 1.png";
+import tvshow2 from "./carousels/tvshow screenshot 2.png";
+import tvshow3 from "./carousels/tvshow screenshot 3.png";
+import metricswebapp1 from "./carousels/metrics webapp screenshot 1.png";
+import metricswebapp2 from "./carousels/metrics webapp screenshot 2.png";
+import recipeapp3 from "./carousels/recipe app screenshot 3.png";
+import recipeapp4 from "./carousels/recipe app screenshot 4.png";
+import recipeapp6 from "./carousels/recipe app screenshot 6.png";
+import trackbuget1 from "./carousels/track budget screenshot 1.png";
+import trackbuget2 from "./carousels/track budget screenshot 2.png";
+import trackbuget5 from "./carousels/track budget screenshot 5.png";
+import carrental3 from "./carousels/car rental screenshot 3.png";
+import carrental4 from "./carousels/car rental screenshot 4.png";
+import carrental6 from "./carousels/car rental screenshot 6.png";
+import ados1 from "./carousels/ados1.jpg";
+import ados2 from "./carousels/ados2.jpg";
+import ados3 from "./carousels/ados3.jpg";
+import locks1 from "./carousels/locks1.jpg";
+import locks2 from "./carousels/locks2.jpg";
+import locks3 from "./carousels/locks3.jpg";
+import runn1 from "./carousels/runn1.jpg";
+import runn2 from "./carousels/runn2.jpg";
+import runn3 from "./carousels/runn3.jpg";
+import eva1 from "./carousels/eva1.png";
+import eva2 from "./carousels/eva2.png";
+import eva3 from "./carousels/eva3.png";
+import priojon1 from "./carousels/priojon1.png";
+import priojon2 from "./carousels/priojon2.png";
+import priojon3 from "./carousels/priojon3.png";
+import letsusedata1 from "./carousels/letsusedata1.png";
+import letsusedata2 from "./carousels/letsusedata2.png";
+import letsusedata3 from "./carousels/letsusedata3.png";
+
+// services
+import responsive from "./services/responsive.gif";
+import react from "./services/react.gif";
+import fullstack from "./services/fullstack.gif";
+
+export {
+  logo,
+  facebook,
+  instagram,
+  github,
+  linkedin,
+  twitter,
+  facebook1,
+  instagram1,
+  github1,
+  xing,
+  xing1,
+  twitter1,
+  linkedin1,
+  tvshowbox,
+  guesstheword,
+  tictactoe,
+  mathmagician,
+  metricwebapp,
+  spacetravelers,
+  recipeapp,
+  trackbudget,
+  carrental,
+  eva,
+  ados,
+  runn,
+  locks,
+  priojon,
+  letsusedata,
+  guesstheword1,
+  guesstheword2,
+  guesstheword3,
+  spacetravelers1,
+  spacetravelers2,
+  spacetravelers3,
+  tictactoe1,
+  tictactoe2,
+  tictactoe3,
+  tvshow1,
+  tvshow2,
+  tvshow3,
+  metricswebapp1,
+  metricswebapp2,
+  recipeapp3,
+  recipeapp4,
+  recipeapp6,
+  trackbuget1,
+  trackbuget2,
+  trackbuget5,
+  carrental3,
+  carrental4,
+  carrental6,
+  eva1,
+  eva2,
+  eva3,
+  ados1,
+  ados2,
+  ados3,
+  runn1,
+  runn2,
+  runn3,
+  locks1,
+  locks2,
+  locks3,
+  priojon1,
+  priojon2,
+  priojon3,
+  letsusedata1,
+  letsusedata2,
+  letsusedata3,
+  responsive,
+  react,
+  fullstack,
+};
